e83ed8236316dae1daa89327ebc4ff1e950792a2
lucasm0ta <lmotaribeiro@gmail.com>
2018-04-06 01:05:37 -0300

    Fix game error handling.

--- src/Game.cpp
+++ src/Game.cpp
@@ -32,8 +32,14 @@ Game::Game(std::string title, int width, int height) {
     // IMG_INIT_PNG
     // IMG_INIT_TIF
 
-    IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF);
-
+	int flags_img = IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF;
+    int init_img = IMG_Init(flags);
+	if (init_img&flags_img != flags_img) {
+		printf("IMG_Init: Failed to init required jpg and png support!\n");
+    	printf("IMG_Init: %s\n", IMG_GetError());
+		SDL_Quit();
+		return;
+	}
     // FLAGS
     // MIX_INIT_FLAC
     // MIX_INIT_MP3
@@ -42,15 +48,33 @@ Game::Game(std::string title, int width, int height) {
     // MIX_INIT_FLUIDSYNTH
     // MIX_INIT_MODPLUG
 
-    Mix_Init(MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MP3 | MIX_INIT_OGG);
-    Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024);
+	int flags_mix = MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MP3 | MIX_INIT_OGG;
+    int init_mix = Mix_Init(flags);
+	if (init_mix&flags_mix != flags_mix) {
+		printf("Mix_Init: Failed to init required ogg and mod support!\n");
+    	printf("Mix_Init: %s\n", Mix_GetError());
+		SDL_Quit();
+		return;
+	}
+
+    if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT,
+		              MIX_DEFAULT_CHANNELS, 1024) == -1) {
+	 	printf("Mix_OpenAudio: %s\n", Mix_GetError());
+		Mix_Quit();
+		SDL_Quit();
+		return;
+	}
+
     Mix_AllocateChannels(32);
 
 	// Start Window
 	window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
 	if (window == nullptr) {
 		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		Mix_CloseAudio();
+		Mix_Quit();
 		SDL_Quit();
+		return;
 	}
 
 	// Start Renderer
@@ -58,7 +82,10 @@ Game::Game(std::string title, int width, int height) {
 	if (renderer == nullptr) {
 		SDL_DestroyWindow(window);
 		std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		Mix_CloseAudio();
+		Mix_Quit();
 		SDL_Quit();
+		return;
 	}
 	srand(time(NULL));
 }
